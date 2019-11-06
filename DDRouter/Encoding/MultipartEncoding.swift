453135931f8f46bc56a2a53344c8152a26345238
Will Rigney <wrigney@deloitte.com.au>
2019-11-06 14:44:06 +1100

    bump down target ios version to 11.0

---
+++ DDRouter/Encoding/MultipartEncoding.swift
@@ -0,0 +1,9 @@
+//
+//  MultipartEncoding.swift
+//  DDRouter
+//
+//  Created by Rigney, Will (AU - Sydney) on 30/9/19.
+//  Copyright © 2019 Rigney, Will (AU - Sydney). All rights reserved.
+//
+
+import Foundation
